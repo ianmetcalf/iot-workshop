573a5138ab9816c9a8a1e17638c5c0e870ff12c7
Ian Metcalf <ian@metcalfbuilt.com>
2018-04-18 15:06:29 -0400

    Fixed battery example for WICED board

--- examples/DisplayBattery/DisplayBattery.ino
+++ examples/DisplayBattery/DisplayBattery.ino
@@ -6,11 +6,35 @@
 
 #include <Adafruit_FeatherOLED.h>
 
+#ifdef ARDUINO_SAMD_FEATHER_M0
+  #define VBAT_PIN A7
+  #define readBatteryVoltage() (analogRead(VBAT_PIN) * 2 * 3.3 / 1024)
+#endif
+
+#ifdef ARDUINO_STM32_FEATHER
+  #define VBAT_PIN PA1
+  #define readBatteryVoltage() (analogRead(VBAT_PIN) * 2 * 3.3 / 4096)
+#endif
+
+#ifdef ARDUINO_ESP8266_ESP12
+  #define VBAT_PIN A0
+  #define readBatteryVoltage() (analogRead(VBAT_PIN) * 2 * 3.3 / 1024)
+#endif
+
+#ifdef ARDUINO_FEATHER_ESP32
+  #define VBAT_PIN A13
+  #define readBatteryVoltage() (analogRead(VBAT_PIN) * 2 * 3.3 / 4096)
+#endif
+
 Adafruit_FeatherOLED display = Adafruit_FeatherOLED();
 
 void setup() {
   display.init();
   display.clearDisplay();
+
+  #ifdef ARDUINO_STM32_FEATHER
+  pinMode(VBAT_PIN, INPUT_ANALOG);
+  #endif
 }
 
 void loop() {
@@ -23,32 +47,10 @@ void updateDisplay() {
 
   if (now - last >= 1000) {
     display.clearDisplay();
-    display.setBattery(readBatteryVolts());
+    display.setBattery(readBatteryVoltage());
     display.renderBattery();
     display.display();
 
     last = now;
   }
-}
-
-float readBatteryVolts() {
-  float voltage = 0.0;
-
-  #ifdef ARDUINO_SAMD_FEATHER_M0
-  voltage = analogRead(A7) * 2 * 3.3 / 1024;
-  #endif
-
-  #ifdef ARDUINO_STM32_FEATHER
-  voltage = analogRead(PA1) * 2 * 3.3 / 4096;
-  #endif
-
-  #ifdef ARDUINO_ESP8266_ESP12
-  voltage = analogRead(A0) * 2 * 3.3 / 1024;
-  #endif
-
-  #ifdef ARDUINO_FEATHER_ESP32
-  voltage = analogRead(A13) * 2 * 3.3 / 4096;
-  #endif
-
-  return voltage;
 }
